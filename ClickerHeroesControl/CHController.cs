b1cddec582bcb9d69b154b189caedf7592e9f037
InvisiblePanda <jbmaier@web.de>
2015-10-12 21:33:39 +0200

    Added the ability to send Key+Click combinations

--- ClickerHeroesControl/CHController.cs
+++ ClickerHeroesControl/CHController.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using static ClickerHeroesControl.WinApiConstants;
 
 namespace ClickerHeroesControl
 {
@@ -12,6 +13,13 @@ namespace ClickerHeroesControl
 	{
 		private const string ClickerHeroesWindowTitle = "Clicker Heroes";
 		private readonly IntPtr targetHandle;
+
+		private static readonly IntPtr fish1Position = GetPosition(865, 480);
+		private static readonly IntPtr fish2Position = GetPosition(740, 400);
+		private static readonly IntPtr fish3Position = GetPosition(1050, 410);
+		private static readonly IntPtr fish4Position = GetPosition(750, 350);
+		private static readonly IntPtr fish5Position = GetPosition(740, 400); // has to be found out still
+		private static readonly IntPtr fishOutOfFieldPosition = GetPosition(515, 490);
 
 		public CHController()
 		{
@@ -34,11 +42,11 @@ namespace ClickerHeroesControl
 		{
 			while (!ct.IsCancellationRequested)
 			{
-				TargetClick(700, 500);
-				TargetClick(700, 500);
-				TargetClick(700, 500);
-				TargetClick(700, 500);
-				TargetClick(700, 500);
+				TargetClick(fish1Position);
+				TargetClick(fish2Position);
+				TargetClick(fish3Position);
+				TargetClick(fish4Position);
+				TargetClick(fish5Position);
 
 				try
 				{
@@ -50,11 +58,22 @@ namespace ClickerHeroesControl
 			}
 		}
 
-		public void TargetClick(uint x, uint y)
+		public void TargetClick(IntPtr position)
 		{
-			IntPtr coords = (IntPtr)((y << 16) | x);
-			PostMessage(targetHandle, WinApiConstants.WM_LBUTTONDOWN, (IntPtr)1, coords);
-			PostMessage(targetHandle, WinApiConstants.WM_LBUTTONUP, (IntPtr)0, coords);
+			PostMessage(targetHandle, WinApiConstants.WM_LBUTTONDOWN, (IntPtr)1, position);
+			PostMessage(targetHandle, WinApiConstants.WM_LBUTTONUP, (IntPtr)0, position);
+		}
+
+		private void KeyPlusClick(uint keyCode, IntPtr position)
+		{
+			PostMessage(targetHandle, WM_KEYDOWN, (IntPtr)keyCode, IntPtr.Zero);
+			TargetClick(position);
+			PostMessage(targetHandle, WM_KEYUP, (IntPtr)keyCode, IntPtr.Zero);
+		}
+
+		private static IntPtr GetPosition(uint x, uint y)
+		{
+			return (IntPtr)((y << 16) | x);
 		}
 
 		#endregion
--- ClickerHeroesControl/WinApiConstants.cs
+++ ClickerHeroesControl/WinApiConstants.cs
@@ -2,7 +2,13 @@
 {
 	public static class WinApiConstants
 	{
-		public const int WM_LBUTTONDOWN = 0x201;
-		public const int WM_LBUTTONUP = 0x202;
+		public const uint WM_LBUTTONDOWN = 0x201;
+		public const uint WM_LBUTTONUP = 0x202;
+		public const uint WM_KEYDOWN = 0x100;
+		public const uint WM_KEYUP = 0x101;
+
+		public const uint VK_SHIFT = 0x10;
+		public const uint VK_CONTROL = 0x11;
+		public const uint KEY_Q = 0x51;
 	}
 }
