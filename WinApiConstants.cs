2d8f3144acebf60ccb2ce4213d7c225b4ccc9129
InvisiblePanda <jbmaier@web.de>
2015-11-11 20:44:34 +0100

    Removed some unused clutter

--- WinApiConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿//namespace ClickerHeroesControl
-//{
-//	//public static class WinApiConstants
-//	//{
-//	//	public const int WM_LBUTTONDOWN = 0x201;
-//	//	public const int WM_LBUTTONUP = 0x202;
-//	//}
-//}
